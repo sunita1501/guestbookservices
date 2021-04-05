bd7d61f0488be262c56a96accd0ce1888a7f5ee6
sunita1501 <sunita.kumari@cognizant.com>
2021-04-05 19:26:47 -0400

    first commit

---
+++ guestbookservice/src/test/java/com/guestbook/guestbookservice/GuestbookserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guestbook.guestbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
