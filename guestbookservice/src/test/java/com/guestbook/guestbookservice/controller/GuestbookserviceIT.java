3eb02f0b44f267ca4adbe0b9b3327fa42748df75
sunita1501 <sunita.kumari@cognizant.com>
2021-04-05 19:54:44 -0400

    Red on post visitor

---
+++ guestbookservice/src/test/java/com/guestbook/guestbookservice/controller/GuestbookserviceIT.java
@@ -0,0 +1,43 @@
+package com.guestbook.guestbookservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GuestbookserviceIT {
+
+    @Autowired
+    MockMvc mockmvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void postGuest() {
+
+        guestDTO heroDTO = new guestDTO("Guest1","Comment1");
+
+        mockmvc.perform(post("/guestbook/guest")
+                .content(objectMapper.writeValueAsString(guestDTO))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        mockmvc.perform(get("/guestbook/guest")).andExpect(status().isOk())
+                .andExpect(jsonPath("[0].heroName").value("Batman"))
+                .andExpect(jsonPath("[0].realName").value("Amir"));
+
+
+    }
+
+}
