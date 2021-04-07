4afe8cc2d82869efaede51d41c824e11a56426ba
574670 <makaeron20@gmail.com>
2021-04-07 18:32:30 -0400

    andDo Green for get call

--- guestbookservice/src/test/java/com/guestbook/guestbookservice/controller/GuestbookIT.java
+++ guestbookservice/src/test/java/com/guestbook/guestbookservice/controller/GuestbookIT.java
@@ -3,11 +3,17 @@ package com.guestbook.guestbookservice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.beans.BeanProperty;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,7 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //import static sun.security.krb5.internal.KDCOptions.with;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc//(addFilters = false)
+@AutoConfigureRestDocs
 public class GuestbookIT {
 
     @Autowired
@@ -24,19 +31,29 @@ public class GuestbookIT {
     @Test
     public void postGuest() throws Exception {
 
-        GuestDTO guestDTO  = new GuestDTO("Guest1","Comment1");
+        GuestDTO guestDTO = new GuestDTO("Guest1", "Comment1");
         ObjectMapper objectMapper = new ObjectMapper();
         mockmvc.perform(post("/guest")
                 .content(objectMapper.writeValueAsString(guestDTO))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
 
-        //mockmvc.perform(get("/guest")).andExpect(status().isOk())
-          //      .andExpect(jsonPath("[0].name").value("Guest1"));
-
-
-
-
     }
 
+    @Test
+    public void getGuest() throws Exception{
+
+        GuestDTO guestDTO = new GuestDTO("Guest1", "Comment1");
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockmvc.perform(post("/guest")
+                .content(objectMapper.writeValueAsString(guestDTO))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        mockmvc.perform(get("/guest")).andExpect(status().isOk())
+                .andExpect(jsonPath("[0].name").value("Guest1"))
+                .andDo(document("Books", responseFields(
+                        fieldWithPath("[0].name").description("First Guest"),
+                        fieldWithPath("[0].comment").description("First Guest Comment"))));
+    }
 }
